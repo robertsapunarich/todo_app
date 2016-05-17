e07262d347173f1f27efa1be5a600f12ac7996f6
Robert Sapunarich <robertsapunarich@gmail.com>
2016-05-17 14:16:41 -0700

    Write spec for `update` action in `tasks_controller`. Add `update` action and `task_params` to `tasks_controller` so spec passes. When a task is checked, jQuery checks for the `id` of the task and its `doneValue`, logs it to the console, and makes a `PUT` request to the database to update its `doneValue`.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,16 @@ class TasksController < ApplicationController
     render :json => Task.all
   end
 
+  def update
+    task = Task.find(params[:id])
+    task.update_attributes(task_params)
+    render json: task
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:done)
+  end
+
 end
