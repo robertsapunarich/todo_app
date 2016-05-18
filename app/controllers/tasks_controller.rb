b3e3e9f07250aebf6bd364d98b2c31d48ffec648
Robert Sapunarich <robertsapunarich@gmail.com>
2016-05-18 15:20:52 -0700

    Write tests for `tasks_controller` `create` action. Add `create` action to render JSON data for `task`. Update `task_params` to permit `:title` of task.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,11 @@ class TasksController < ApplicationController
     render :json => Task.order(:id)
   end
 
+  def create
+    task = Task.create(task_params)
+    render json: task
+  end
+
   def update
     task = Task.find(params[:id])
     task.update_attributes(task_params)
@@ -13,7 +18,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:done)
+    params.require(:task).permit(:done, :title)
   end
 
 end
